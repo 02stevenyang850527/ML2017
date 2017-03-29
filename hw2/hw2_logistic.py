68c1cf8eb053c874dbd5dda837204548a12cba08
Steven <yangqisheng@yangqishengde-MacBook-Pro.local>
2017-03-29 20:41:55 +0800

    over strong

--- hw2/hw2_logistic.py
+++ hw2/hw2_logistic.py
@@ -9,17 +9,36 @@ def sigmoid(x):
 
 #-------read data--------
 feature = pd.read_csv('X_train')
+raw_data = pd.read_csv('train.csv',header=None)
+feature_add = np.array(raw_data.iloc[:,4])
+feature_add = np.concatenate((feature_add.reshape(-1,1)**2,(feature_add.reshape(-1,1)**3)),axis=1)
+del feature['fnlwgt']
 feature_np = np.array(feature)
+#feature_np = feature_np[:,:65]
+feature_np = np.concatenate((feature_np,feature_add),axis=1)
+feature_np = np.concatenate((feature_np,(feature_np[:,0].reshape(-1,1))**2),axis=1)
+feature_np = np.concatenate((feature_np,(feature_np[:,0].reshape(-1,1))**3),axis=1)
+feature_np = np.concatenate((feature_np,(feature_np[:,2].reshape(-1,1))**2),axis=1)
+feature_np = np.concatenate((feature_np,(feature_np[:,2].reshape(-1,1))**3),axis=1)
+feature_np = np.concatenate((feature_np,(feature_np[:,3].reshape(-1,1))**2),axis=1)
 mean = feature_np.mean(0)
 std = feature_np.std(0)
 feature_np = (feature_np-mean)/std
 
-#raw_data = pd.read_csv('train.csv',header=None)
-#data = np.array(raw_data)
 train_result = np.loadtxt('Y_train')
-# cuba = np.array(feature[' Cuba'])
 test_data = pd.read_csv('X_test')
+test_raw = pd.read_csv('test.csv')
+test_add = np.array(test_raw.iloc[:,4])
+test_add = np.concatenate((test_add.reshape(-1,1)**2,(test_add.reshape(-1,1))**3),axis=1)
+del test_data['fnlwgt']
 test_np = np.array(test_data)
+test_np = np.concatenate((test_np,test_add),axis=1)
+test_np = np.concatenate((test_np,(test_np[:,0].reshape(-1,1))**2),axis=1)
+test_np = np.concatenate((test_np,(test_np[:,0].reshape(-1,1))**3),axis=1)
+test_np = np.concatenate((test_np,(test_np[:,2].reshape(-1,1))**2),axis=1)
+test_np = np.concatenate((test_np,(test_np[:,2].reshape(-1,1))**3),axis=1)
+test_np = np.concatenate((test_np,(test_np[:,3].reshape(-1,1))**2),axis=1)
+#test_np = test_np[:,:65]
 test_np = (test_np-mean)/std
 #------- train ----------
 
@@ -28,12 +47,13 @@ w = np.array([0.0]*feature_np.shape[1])
 lr = 0.5
 b_lr = 0.0
 w_lr = np.array([0.0]*feature_np.shape[1])
+lamda = 15  # For Regularization
 iteration = 2000
 
 for i in range(iteration):
     scalar_mat = train_result - sigmoid(b + np.dot(feature_np,w))
     b_grad = -np.sum(scalar_mat)
-    w_grad = -np.dot(scalar_mat,feature_np)
+    w_grad = -np.dot(scalar_mat,feature_np) + lamda*w
     b_lr = b_lr + b_grad**2
     w_lr = w_lr + w_grad**2
     # Update parameters
