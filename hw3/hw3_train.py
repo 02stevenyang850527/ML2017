6a48e77233c528ea0a7e1ab67c6b5f4b970c3957
Steven <yangqisheng@yangqishengde-MacBook-Pro.local>
2017-04-13 17:48:03 +0800

    over the strong baseline

--- hw3/hw3_train.py
+++ hw3/hw3_train.py
@@ -5,6 +5,8 @@ from keras.layers.core import Dense, Dropout, Activation
 from keras.layers import Convolution2D, MaxPooling2D, Flatten
 from keras.optimizers import SGD, Adam
 from keras.utils import np_utils
+from keras.callbacks import EarlyStopping
+from keras.preprocessing.image import ImageDataGenerator
 
 
 num_classes = 7
@@ -17,26 +19,21 @@ train_feature = train_feature/255
 train_label = np.array(train['label'])
 
 #--------training & validation data--------
-#valid_amount = 5000
-#valid_feature = train_feature[:valid_amount]
-#valid_label = train_label[:valid_amount]
+valid_num = 5000
+valid_feature = train_feature[:valid_num]
+valid_label = train_label[:valid_num]
 
-#train_feature = np.delete(train_feature,range(valid_amount),axis=0)
-#train_label = np.delete(train_label,range(valid_amount),axis=0)
+train_feature = train_feature[valid_num:]
+train_label = train_label[valid_num:]
+
 train_feature = train_feature.reshape(train_feature.shape[0],48,48,1)
-#valid_feature = valid_feature.reshape(valid_amount,48,48,1)
-
+valid_feature = valid_feature.reshape(valid_feature.shape[0],48,48,1)
 train_label = np_utils.to_categorical(train_label,num_classes)
-#valid_label = np_utils.to_categorical(valid_label,num_classes)
+valid_label = np_utils.to_categorical(valid_label,num_classes)
 
 #-------- CNN ------------
 model = Sequential()
-model.add(Convolution2D(32,(3,3),input_shape=(48,48,1)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(2,2))
-model.add(Dropout(0.25))
-
-model.add(Convolution2D(32,(3,3)))
+model.add(Convolution2D(64,(3,3),input_shape=(48,48,1)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(2,2))
 model.add(Dropout(0.25))
@@ -46,10 +43,15 @@ model.add(Activation('relu'))
 model.add(MaxPooling2D(2,2))
 model.add(Dropout(0.25))
 
-model.add(Flatten())
-model.add(Dense(512))
+model.add(Convolution2D(256,(3,3)))
 model.add(Activation('relu'))
-model.add(Dropout(0.4))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(0.3))
+
+model.add(Flatten())
+model.add(Dense(1024))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 
 model.add(Dense(num_classes))
 model.add(Activation('softmax'))
@@ -57,12 +59,32 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adamax',metrics=['accuracy'])
 
-#model.fit(train_feature, train_label, validation_data=(valid_feature, valid_label), batch_size=30,epochs=10)
-model.fit(train_feature, train_label, validation_split = 0.2, batch_size=64,epochs=35)
+#early_stopping = EarlyStopping(monitor = 'val_loss', patience=2)
 
-print ('fit')
+datagen = ImageDataGenerator(
+        featurewise_center=False,               # set input mean to 0 over the dataset
+        samplewise_center=False,                # set each sample mean to 0
+        featurewise_std_normalization=False,    # divide inputs by std of the dataset
+        samplewise_std_normalization=False,     # divide each input by its std
+        zca_whitening=False,                    # apply ZCA whitening
+        rotation_range=3,                       # randomly rotate images in the range (degrees, 0 to 180)
+        width_shift_range=0.1,                  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,                 # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,                   # randomly flip images
+        vertical_flip=False)                    # randomly flip images
+
+datagen.fit(train_feature)
+
+batch = 64
+for i in range(4):
+    model.fit(train_feature, train_label,validation_data=(valid_feature,valid_label), batch_size=batch,epochs=6)
+    model.fit_generator(datagen.flow(train_feature,train_label,batch_size=batch),
+                        steps_per_epoch = train_feature.shape[0]/batch,
+                        epochs = 4,
+                        validation_data = (valid_feature, valid_label)
+                       )
+
 score = model.evaluate(train_feature,train_label)
-print ('Train Acc: ', score[1])
+print ('\nTrain Acc: ', score[1])
 
-model.save('my_model.h5')
-print ('saved')
+model.save('my_model_2.h5')
