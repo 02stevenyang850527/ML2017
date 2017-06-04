661db226ce2b4a3974cdb1341f06b0e6f7a471d1
Steven <yangqisheng@yangqishengde-MacBook-Pro.local>
2017-06-04 22:22:15 +0800

    mf version

--- hw6/test_mf.py
+++ hw6/test_mf.py
@@ -1,17 +1,27 @@
-import numpy as np
 import pandas as pd
+import numpy as np
+from keras.models import load_model
+import keras.backend as K
+
+
+def rmse(y_true,y_pred):
+    return K.sqrt(K.mean((y_pred - y_true)**2))
 
 
 test_path = 'test.csv'
 output_path = 'ans_mf.csv'
 
 test = pd.read_csv(test_path)
-pred = np.load('pred.npy')
-query = (test['UserID'].values-1,test['MovieID'].values-1)
-result = pred[query]
 
-out_txt = 'TestDataID,Rating\n'
+user = test['UserID'].values
+movie = test['MovieID'].values
+
+model = load_model('mf.h5', custom_objects={'rmse':rmse})
+result = model.predict([user, movie], batch_size=128, verbose=1)
+result = result.reshape(len(result))
+
 of = open(output_path,'w')
+out_txt = 'TestDataID,Rating\n'
 for i in range(len(result)):
     out_txt += str(i+1) +','+ str(result[i]) + '\n'
 
